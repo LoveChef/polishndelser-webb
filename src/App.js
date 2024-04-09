1d22b25995f0fca773e9504f7a79b807b2776e91
LoveChef <117826056+LoveChef@users.noreply.github.com>
2024-04-09 09:51:27 +0200

    Jalla

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <h1>Polishändelser</h1>
+      </header>
+
+      <button class="button-events">
+        <span>Händelser</span>
+      </button>
+    </div>
+  );
+}
+
+export default App;
