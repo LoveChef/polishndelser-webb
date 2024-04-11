76f161f6d477d7cc392413864117df48ca5b1d97
LoveChef <117826056+LoveChef@users.noreply.github.com>
2024-04-11 08:50:59 +0200

    Design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.png';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [today, setToday] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setToday(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const options = { weekday: 'long' }; // Ange options för att visa veckodag
+  const weekday = today.toLocaleDateString('sv-SE', options); // Visa veckodagen på svenska
+
   return (
-    <div className="App">
+    <><div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <h1>Polishändelser</h1>
@@ -13,6 +28,12 @@ function App() {
         <span>Händelser</span>
       </button>
     </div>
+    <footer className="footer">
+      <p id="title-p">Love Ekelöw</p>
+      <p>All rights reserved</p>
+      <p>Ha en trevlig {weekday}!</p>
+    </footer></>
+    
   );
 }
 
